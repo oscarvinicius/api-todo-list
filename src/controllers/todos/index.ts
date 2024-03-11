b41ec24af199f55fdd22d2ac72c648a3e9306c6f
Oscar Vinicius <oscarvinicius1999@gmail.com>
2024-03-11 14:03:55 -0300

    Add list, show, update and remove methods

--- src/controllers/todos/index.ts
+++ src/controllers/todos/index.ts
@@ -1,5 +1,13 @@
 import * as create from "./create";
+import * as list from "./list";
+import * as show from "./show";
+import * as update from "./update";
+import * as remove from "./remove";
 
 export const TodosController = {
   ...create,
+  ...list,
+  ...show,
+  ...update,
+  ...remove,
 };
--- src/server/routes.ts
+++ src/server/routes.ts
@@ -2,10 +2,10 @@ import { Router } from "express";
 import { TodosController } from "../controllers";
 const router = Router();
 
-router.get("/teste", (req, res) => {
-  console.log("teste ok");
-});
-
-router.post("/", TodosController.create);
+router.get("/todos", TodosController.list);
+router.get("/todos/:id", TodosController.show);
+router.post("/todos", TodosController.create);
+router.put("/todos/:id", TodosController.update);
+router.delete("/todos/:id", TodosController.remove);
 
 export { router };
